1380a9c616afe0752eab95bb789cbb2af04cdde9
vetusmagnus <111835005+vetusmagnus@users.noreply.github.com>
2026-09-24 03:29:00 -0400

    localdecomp: match func_0039A5C8

--- src/text.c
+++ src/text.c
@@ -1324,7 +1324,7 @@ void func_0039A5A8(void) {
 /* localdecomp:end func_0039A5A8 */
 
 /* localdecomp:start func_0039A5C8 */
-extern void func_0039A550(s32);
+extern func_0039A550(s32);
 void func_0039A5C8(void) {
     func_0039A550(0x22);
 }
