0035b3cc5a09eae582eb7e2122333ff6343d8fa5
vetusmagnus <111835005+vetusmagnus@users.noreply.github.com>
2026-09-25 13:31:20 -0400

    localdecomp: match func_003E82B8

--- src/text.c
+++ src/text.c
@@ -7822,6 +7822,7 @@ s32 func_003E8248(u8 *p, f32 a, f32 b) {
 /* localdecomp:end func_003E8248 */
 
 /* localdecomp:start func_003E82B8 */
+typedef struct { u8 pad[0x10]; s32 (*isA)(void *, s32); } VT_E;
 extern s32 D_001D9740_g;
 extern void func_003E8610();
 s32 func_003E82B8(u8 *p, s32 a, s32 b) {
