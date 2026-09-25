624199dd70555386e39ff8a687d5ef91829dec24
vetusmagnus <111835005+vetusmagnus@users.noreply.github.com>
2026-09-25 13:53:59 -0400

    localdecomp: match func_003E7960

--- src/text.c
+++ src/text.c
@@ -7611,6 +7611,8 @@ s32 func_003E78B0(u8 *p, s32 a) {
 INCLUDE_ASM("asm/nonmatchings/text", func_003E7900);
 
 /* localdecomp:start func_003E7960 */
+typedef struct { u32 key; void *val; } HE_8;
+typedef struct { s32 f0; s32 n; HE_8 e[8]; } HT_8;
 extern u8 D_001DAA97;
 extern void *func_003E4EA8(HT_8 *, u32);
 s32 func_003E7960(HT_8 *t, u32 key, void *val) {
