8b4b2276921f3b69a630a012682b20db147ba7d6
vetusmagnus <111835005+vetusmagnus@users.noreply.github.com>
2026-09-25 13:48:38 -0400

    localdecomp: match func_003E5028

--- src/text.c
+++ src/text.c
@@ -6748,6 +6748,8 @@ void *func_003E4FA8(HT_8 *t, u32 key) {
 /* localdecomp:end func_003E4FA8 */
 
 /* localdecomp:start func_003E5028 */
+typedef struct { u32 key; void *val; } HE_8;
+typedef struct { s32 f0; s32 n; HE_8 e[8]; } HT_8;
 extern u8 D_001DAA9A;
 void *func_003E5028(HT_8 *t, u32 key) {
     s32 i;
