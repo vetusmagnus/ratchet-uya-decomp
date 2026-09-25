35a5b355297e3cf856d7b524a2cb8e5e0a0ada01
vetusmagnus <111835005+vetusmagnus@users.noreply.github.com>
2026-09-25 13:35:03 -0400

    localdecomp: match func_003E5CA0

--- src/text.c
+++ src/text.c
@@ -6987,6 +6987,7 @@ s32 func_003E5BD0(HT_8 *t, u32 key, void *val) {
 /* localdecomp:end func_003E5BD0 */
 
 /* localdecomp:start func_003E5CA0 */
+typedef struct { u8 pad[0x10]; s32 (*isA)(void *, s32); } VT_E;
 extern s32 D_001D97D0[];
 s32 func_003E5CA0(u8 *p, f32 a, f32 b) {
     if ((*(VT_E **)(p + 8))->isA(p, D_001D97D0[0])) {
@@ -7001,6 +7002,7 @@ s32 func_003E5CA0(u8 *p, f32 a, f32 b) {
 INCLUDE_ASM("asm/nonmatchings/text", func_003E5D08);
 
 /* localdecomp:start func_003E5DE0 */
+typedef struct { s32 f0; s32 n; HE_8 e[8]; } HT_8;
 extern u8 D_001DAA91;
 extern void *func_003E4BA0(HT_8 *, u32);
 s32 func_003E5DE0(HT_8 *t, u32 key, void *val) {
